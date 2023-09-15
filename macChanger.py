bbc97c88df1bef55bae3a129a41d8bf8da298e7f
yigit-nazligul <yigitnazligul-sw@hotmail.com>
2023-09-15 18:44:33 +0300

    Update macChanger.py

--- macChanger.py
+++ macChanger.py
@@ -1,16 +1,22 @@
 import subprocess
 import optparse
 
-parse_object = optparse.OptionParser()
-parse_object.add_option("-i","--interface",dest = "interface",help= "Interface to you want to change.")
-parse_object.add_option("-m","--mac",dest = "mac_adress",help = "Please type your new MAC adress.")
-
-print ("Mac Changer Started !")
-
-subprocess.call(["ifconfig",interface,"down"])
-subprocess.call(["ifconfig",interface,"hw","ether",mac_adress])
-subprocess.call(["ifconfig",interface,"up"])
+def get_user_input():
+    parse_object = optparse.OptionParser()
+    parse_object.add_option("-i","--interface",dest = "interface",help= "Interface to you want to change.")
+    parse_object.add_option("-m","--mac",dest = "mac_address",help = "Please type your new MAC adress.")
+    return parse_object.parse_args
 
 
+def change_mac_address(interface,mac_address):
+    subprocess.call(["ifconfig",interface,"down"])
+    subprocess.call(["ifconfig",interface,"hw","ether",mac_adress])
+    subprocess.call(["ifconfig",interface,"up"])
 
+print ("MAC Address Changer Started!")
+get_user_input() = (user_input,arguments)
+change_mac_address = (user_input.interface,user_input.mac_address)
 
+def check_new_mac():
+    ifconfig = subprocess.check_output(["ifconfig",interface])
+    print(ifconfig)
